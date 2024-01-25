aecb052f341ed0c69d34bf7fe74cc9a54b95c93e
keanknowsss <solomon.keanu.sk@gmail.com>
2024-01-26 07:58:09 +0800

    add joke category

--- index.js
+++ index.js
@@ -12,16 +12,38 @@ app.use(express.static("public"));
 // Routes
 app.get("/", async (req, res) => {
 	try {
-		const response = await axios.get("any", {
+		const response = await axios.get("/Any", {
 			baseURL: JOKES_API_URL,
 			params: {
-				type: "single"
-			}
+				type: "single",
+				blacklistFlags: "nsfw,religious,political,racist,sexist,explicit",
+				// idRange: 28 // long joke, handle in styling
+				// add handler for twopart
+			},
 		});
-		console.log(response["data"]);
-		res.render("index.ejs", { joke: response["data"] });
+		let joke = response['data']
+		let categories = [joke['category']];
+		
+		if (joke['flags']['nsfw'])
+			categories.push('Nsfw');
+		if (joke['flags']['religious'])
+			categories.push('Religious');
+		if (joke['flags']['political'])
+			categories.push('Political');
+		if (joke['flags']['sexist'])
+			categories.push('Sexist');
+		if (joke['flags']['explicit'])
+			categories.push('Explicit');
+
+		// console.log(response["data"]);
+		console.log(categories);
+		res.render("index.ejs", { joke, categories });
 	} catch (error) {
-		console.error(error);
+		if (error['code'] === 'ENOTFOUND') {
+			console.log('no joke') // TODO: handle error
+		} else {
+			console.error(error);
+		}
 		return res.sendStatus(500);
 	}
 });
